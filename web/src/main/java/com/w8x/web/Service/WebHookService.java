f1ba35af9859d34b5fa1a80f3bb75a50487e7f7e
kang <2601624788@qq.com>
2019-04-17 22:33:36 +0800

    three

---
+++ web/src/main/java/com/w8x/web/Service/WebHookService.java
@@ -0,0 +1,5 @@
+package com.w8x.web.Service;
+
+public interface WebHookService {
+
+}
---
+++ web/src/main/java/com/w8x/web/Service/impl/WebHookServiceImpl.java
@@ -0,0 +1,9 @@
+package com.w8x.web.Service.impl;
+
+import com.w8x.web.Service.WebHookService;
+
+public class WebHookServiceImpl implements WebHookService {
+    private void solveJson(String json){
+
+    }
+}
