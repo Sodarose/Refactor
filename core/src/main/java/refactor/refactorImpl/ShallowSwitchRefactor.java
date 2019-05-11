672a65437156a9e80e7bd0f1881419c9e7cd9635
kangkang <2601624788@qq.com>
2019-05-11 20:58:15 +0800

    switch转if语句

--- core/src/main/java/refactor/refactorImpl/ShallowSwitchRefactor.java
+++ core/src/main/java/refactor/refactorImpl/ShallowSwitchRefactor.java
@@ -10,48 +10,67 @@ import refactor.AbstractRefactor;
 import java.util.*;
 
 /**
- *
- * */
+ * 将case小于等于3的switch语句转换为if
+ */
 public class ShallowSwitchRefactor extends AbstractRefactor {
 
     @Override
     public ReCorrect refactor(Issue issue) {
         SwitchStmt switchStmt = (SwitchStmt) issue.getIssueNode();
+        transFrom(switchStmt);
         return null;
     }
 
-    private void  transFrom(SwitchStmt switchStmt){
-
+    private void transFrom(SwitchStmt switchStmt) {
+        IfStmt ifStmt = buildStmt(switchStmt.getSelector(), switchStmt.getEntries());
+        try {
+            BlockStmt blockStmt = (BlockStmt) switchStmt.getParentNode().get();
+            int index = blockStmt.getStatements().indexOf(switchStmt);
+            blockStmt.getStatements().add(index, ifStmt);
+            blockStmt.remove(switchStmt);
+            System.out.println(ifStmt);
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
      * 得到转换后的列表
-     * */
-    private IfStmt buildStmt(Expression selector, List<SwitchEntry> switchEntries){
-        IfStmt p = null;
+     */
+    private IfStmt buildStmt(Expression selector, List<SwitchEntry> switchEntries) {
+        IfStmt p = null, q = null;
         boolean head = true;
-        for(SwitchEntry switchEntry:switchEntries){
-            Expression condition = buildCondition(selector,switchEntry.getLabels().get(0));
-            Statement statement = buildIfStmt(condition,switchEntry);
-            if(statement.isIfStmt()){
-                if(head){
-
+        for (SwitchEntry switchEntry : switchEntries) {
+            Expression condition = buildCondition(selector, switchEntry.getLabels().get(0));
+            Statement statement = buildIfStmt(condition, switchEntry);
+            if (statement.isIfStmt()) {
+                if (head) {
+                    p = statement.asIfStmt();
+                    q = p;
+                    continue;
                 }
+                p.setElseStmt(statement.asIfStmt());
+                p = statement.asIfStmt();
+            }
+            if (statement.isBlockStmt()) {
+                p.setElseStmt(statement.asBlockStmt());
             }
         }
-        return null;
+        return q;
     }
 
     /**
      * 处理列表
-     * */
-    private List<Statement> sloveStateList(Map<SwitchEntry,Statement> map){
+     */
+    private List<Statement> sloveStateList(Map<SwitchEntry, Statement> map) {
         List<Statement> statements = new ArrayList<>();
-
         return statements;
     }
 
-    private Expression buildCondition(Expression select,Expression expression){
+    /**
+     * 生成表达式
+     */
+    private Expression buildCondition(Expression select, Expression expression) {
         BinaryExpr condition = new BinaryExpr();
         condition.setOperator(BinaryExpr.Operator.EQUALS);
         condition.setLeft(select);
@@ -61,11 +80,11 @@ public class ShallowSwitchRefactor extends AbstractRefactor {
 
 
     /**
-     * @param condition if条件
+     * @param condition   if条件
      * @param switchEntry case 分支
-     * */
-    private Statement buildIfStmt(Expression condition, SwitchEntry switchEntry){
-        if(isDefault(switchEntry)){
+     */
+    private Statement buildIfStmt(Expression condition, SwitchEntry switchEntry) {
+        if (isDefault(switchEntry)) {
             BlockStmt blockStmt = new BlockStmt();
             blockStmt.getStatements().addAll(switchEntry.getStatements());
             return blockStmt;
@@ -78,21 +97,24 @@ public class ShallowSwitchRefactor extends AbstractRefactor {
         return ifStmt;
     }
 
-    private boolean cheakBreakOrReturn(SwitchEntry switchEntry){
+    /**
+     * 检测是否含有break或者return
+     */
+    private boolean cheakBreakOrReturn(SwitchEntry switchEntry) {
         List<Statement> statements = switchEntry.getStatements();
-        for(Statement statement:statements){
-            if(statement.isBreakStmt()){
+        for (Statement statement : statements) {
+            if (statement.isBreakStmt()) {
                 return true;
             }
-            if(statement.isReturnStmt()){
+            if (statement.isReturnStmt()) {
                 return true;
             }
         }
         return false;
     }
 
-    private boolean isDefault(SwitchEntry switchEntry){
-        return switchEntry.getLabels().get(0)==null?true:false;
+    private boolean isDefault(SwitchEntry switchEntry) {
+        return switchEntry.getLabels().get(0) == null ? true : false;
     }
 
 }
---
+++ core/src/test/java/FindNodeUlits.java
@@ -0,0 +1,8 @@
+import com.github.javaparser.ast.Node;
+
+/**
+ * 快速查找
+ * */
+public class FindNodeUlits<T extends Node> {
+
+}
